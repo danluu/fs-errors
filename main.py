b30fa7c4f8da94bbef6b5ec5d772f88a49f9effc
Dan Luu <danluu@gmail.com>
2017-10-02 19:46:08 -0400

    Refactor dmsetup table code into function.

--- main.py
+++ main.py
@@ -83,23 +83,28 @@ def get_device_size(loopback_name):
     return device_size
 
 
+# Calculate dmsetup table
+def get_dmsetup_table(error_block):
+    dm_table = """\
+    0 {error_start} linear /dev/loop0 0
+    {error_start} {error_size} error
+    {linear_start} {linear_end_size} linear /dev/loop0 {linear_start}""".format(
+        error_start=error_block[0],
+        error_size=error_block[1],
+        linear_start=sum(error_block),
+        linear_end_size=device_size-sum(error_block))
+
+    return dm_table
+
+
 image_path, filesystem_md5sum = get_args()
 error_block = (2389, 1) #TODO(Wesley) multi-section errors
 
 tmp_image_path = make_tmpfile(image_path, filesystem_md5sum)
 loopback_name = make_loopback_device(tmp_image_path)
 device_size = get_device_size(loopback_name)
+dm_table = get_dmsetup_table(error_block)
 
-# Calculate dmsetup table
-
-dm_table = """\
-0 {error_start} linear /dev/loop0 0
-{error_start} {error_size} error
-{linear_start} {linear_end_size} linear /dev/loop0 {linear_start}""".format(
-    error_start=error_block[0],
-    error_size=error_block[1],
-    linear_start=sum(error_block),
-    linear_end_size=device_size-sum(error_block))
 
 # Run dmsetup
 
