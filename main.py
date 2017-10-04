f6f04214eba89b06d94656b3a1c9f155a2d8dc8a
Dan Luu <danluu@gmail.com>
2017-10-04 18:31:55 -0400

    Add debug info and don't always use loop0.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 import os
 
 def exec_command(command, exit_on_error=True):
+    print(command)
     command_result = subprocess.run(command,
                                     stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE)
@@ -83,12 +84,13 @@ def get_device_size(loopback_name):
 
 
 # Calculate dmsetup table
-def get_dmsetup_table(device_size, error_block):
+def get_dmsetup_table(device_size, loop_name, error_block):
     dm_table = """\
-    0 {error_start} linear /dev/loop0 0
+    0 {error_start} linear {loop_name} 0
     {error_start} {error_size} error
-    {linear_start} {linear_end_size} linear /dev/loop0 {linear_start}""".format(
+    {linear_start} {linear_end_size} linear {loop_name} {linear_start}""".format(
         error_start=error_block[0],
+        loop_name=loop_name,
         error_size=error_block[1],
         linear_start=sum(error_block),
         linear_end_size=device_size-sum(error_block))
@@ -146,7 +148,7 @@ def main():
     tmp_image_path = make_tmpfile(image_path, filesystem_md5sum)
     loopback_name = make_loopback_device(tmp_image_path)
     device_size = get_device_size(loopback_name)
-    dm_table = get_dmsetup_table(device_size, error_block)
+    dm_table = get_dmsetup_table(device_size, loopback_name, error_block)
     dm_volume_name = run_dmsetup(dm_table)
     mountpoint = mount_dm_device(dm_volume_name)
     exec_test(mountpoint, image_path)
